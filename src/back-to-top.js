4f0cd6b4670e0a171bf234eebb4c921afc9be4f6
aznoqmous <paul.landreau931v@gmail.com>
2021-01-07 09:28:08 +0100

    added threshold

--- src/back-to-top.js
+++ src/back-to-top.js
@@ -3,23 +3,29 @@ export default class BackToTop {
     this.trigger = trigger
     this.options = Object.assign({
       container: window,
-      speed: 1000 // ms to top
+      speed: 1000, // ms to top
+      threshold: null // trigger element visibility will toggle at this container.scrollY position, up to 100vh if null
     }, options)
 
     this.container = this.options.container
+    if(!this.options.threshold) this.options.threshold = window.innerHeight
 
     this.bind()
   }
 
   bind(){
     this.trigger.addEventListener('click', ()=>{ this.toTop() })
+    this.container.addEventListener('scroll', ()=>{
+      if(this.getContainerScroll() > this.options.threshold) this.trigger.classList.add('active')
+      else this.trigger.classList.remove('active')
+    })
   }
 
   toTop(){
     let startT = Date.now()
     let startY = this.getContainerScroll()
     let speed = startY / this.options.speed
-    
+
     let loop = ()=>{
       let deltaT = (Date.now() - startT) / this.options.speed
       this.container.scrollTo(0, startY - deltaT * startY)
